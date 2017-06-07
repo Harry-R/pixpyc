e3dfc69da5b8d55e1fadcd7d7e490cb505ca00c5
Leonard Rapp <leonardrapp@gmx.de>
2017-06-07 23:41:16 +0200

    improved comments and code style

--- pixelflut.py
+++ pixelflut.py
@@ -3,19 +3,20 @@ import numpy as np
 import PIL
 from PIL import Image
 
+# server settings
 HOST = 'rgbmatrix'
 PORT = 9737
-
 length = 100;
 height = 100;
+maxsize = (length, height)
 
+# connect socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 send = sock.send
 
-maxsize = (length, height)
-
-img = Image.open('cccknoten.png')
+# prepare image (open, convert to rgba, resize, convert to array)
+img = Image.open('foo.png')
 img = img.convert('RGBA')
 img.thumbnail(maxsize, PIL.Image.ANTIALIAS)
 arr = np.array(img)
@@ -33,16 +34,17 @@ def rect(x,y,w,h,r,g,b):
    for j in xrange(y,y+h):
      setPixel(i,j,r,g,b)
 
-def loop():
-    while 1:
-        # rect(0, 0, 100, 100, 255, 0, 0)
-        image()
-
+# print image array
 def image():
     for i in xrange(0, img.size[0]):
         for j in xrange(0, img.size[1]):
             setPixel(i, j, arr[j][i][0], arr[j][i][1], arr[j][i][2], 255)
 
+# do wahtever
+def loop():
+    while 1:
+        # rect(0, 0, 100, 100, 255, 0, 0)
+        image()
+
 if __name__ == '__main__':
-    # prepareImage()
     loop()
